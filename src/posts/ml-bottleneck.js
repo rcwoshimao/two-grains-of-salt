88c43d2d57939e86334e4303a99dcb5f6461284b
rcwoshimao <mantoumiaoshen@gmail.com>
2025-05-18 11:16:43 -0700

    fixed date

--- src/posts/ml-bottleneck.js
+++ src/posts/ml-bottleneck.js
@@ -3,8 +3,6 @@ export const date = "2024-03-19";
 export const slug = "ml-bottleneck";
 export const content = `# Understanding the actual reason for current bottleneck in ML problem solving
 
-May 14th, 2025 
-
 To most people, the field of ML consists of two broad problems: algorithm and data. The algorithm side requires a good understanding of the problem we are trying to solve, and data side is currently facing a problem: big companies have exhausted all data sources on the internet(some purposed the solution of generating synthetic data), but we don't know if products are getting better. We are facing a bottleneck— it's like there's nothing more we can do, other than waiting for more data to be made. 
 
 A common pitfall here is to think about those two fields as completely separate from each other, as algorithm people are supposed to create some "castle in the air", while the data people trying to fine tune it. Already, we need to amend the knowledge of those two fields. 
