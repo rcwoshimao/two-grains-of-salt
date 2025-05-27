1011a33025cac4a36a2eccafbc5193c30b7bb8db
rcwoshimao <mantoumiaoshen@gmail.com>
2025-05-27 01:01:26 -0700

    added new blog post

---
+++ src/posts/picking-the-right-values.js
@@ -0,0 +1,23 @@
+export const metadata = {
+  title: "Picking the Right Values",
+  slug: "picking-the-right-values",
+  tags: ["Life/Philosophy"],
+  summary: "A thoughtful exploration of values and their impact on our lives.",
+  date: "2024-03-21"
+};
+
+export const content = `
+
+There is a chapter in this one book called **["The Subtle Art of Not Giving a F*ck"](https://nibmehub.com/opac-service/pdf/read/The%20Subtle%20Art%20of%20Not%20Giving%20a%20Fck%20A%20Counterintuitive%20Approach%20to%20Living%20a%20Good%20Life%20by%20Mark%20Manson%20(z-lib.org).pdf)** (or, if we peel off the onion skin of this click-baity book name, we can see the content of the book is more like "How to Give a Fuck about the Right Thing in Life" ), titled "The Value of Suffering", that talked extensively about how values can shape which direction we bring our life to. The section "Rock Star Problem" gave two great examples: Dave Mustaine, who got kicked out of Metallica, suffered through his life even though he was greatly successful in his music career; while Pete Best, who got kicked out of the Beatles, was content with his life with his family despite not being world-famous for his music. This difference only comes from the values they have picked: Dave decided to dedicate his whole life chasing the value of "Being more successful than the band that kicked me out", while Best, after being depressed for quite a bit, picked the value of having a stable marriage instead of fame, while still being able to pursue music as a hobby. 
+
+My life mentor Dr. Chase always said: "Do not fixate on things you cannot control. Instead, focus on the things you can change, regardless of how small that is". It sounds a bit pessimistic at first glance, but it boils down to knowing that there's no reason to brute force an nonexistent solution, while there's something far better to do. There are many things we cannot control; in fact, we cannot control MOST things. This realization is daunting at first, but after we've done weeping for the inevitable, we can always stand up and try to find improvement. This quite correlates to the values we pick. We can either chose to always and always compare ourselves to others and make ourselves miserable (Like Dave, I genuinely wish he is happy now though I do not listen to his music), because we cannot control how successful others are; or, we can chose to evaluate what truly matters, focuing on our own growth and inner happiness disregarding what the outside world deems as successful. (This inner happiness doesn't HAVE to be innovative and different from what the others think it is; at the same time, it also doesn't have to be the same.) 
+
+> Good values are 1) reality-based, 2) socially constructive, and 3) immediate and controllable.  
+> Bad values are 1) superstitious, 2) socially destructive, and 3) not immediate or controllable.  
+>
+> Honesty is a good value because it's something you have complete control over, it reflects reality, and it benefits others (even if it's sometimes unpleasant). Popularity, on the other hand, is a bad value. If that's your value, and if your metric is being the most popular guy/girl at the dance party, much of what happens will be out of your control: you don't know who else will be at the event, and you probably won't know who half those people are.  
+This value/metric isn't based on reality: you may feel popular or unpopular, when in fact you have no fucking clue what anybody else really thinks about you. (Side Note: As a rule, people who are terrified of what others think about them are actually terrified of all the shitty things they think about themselves being reflected back at them.)
+
+
+If we think about this in terms of RRL, we can see that the most important thing isnt to go with one single policy, brutally push through it and find the "best value", but to constantly update our policy so that the way we evaluate our life is better and better. Afterall, having a good policy is for sure much more beneficial in the long term than having a fake good score on a shit policy. 
+`;
