52541aa1eb9b73552a34af53bb8ecd78f2aca05c
rcwoshimao <mantoumiaoshen@gmail.com>
2025-08-20 21:09:37 -0500

    Finished writign spoon theory

--- src/posts/spoon-theory.js
+++ src/posts/spoon-theory.js
@@ -10,35 +10,28 @@ export const content = `
 # The Spoon theory, and how we plan our days
 
 ## 
-On wedesnday afternoon of the last week of my summer internship, I found myself rotting on my bed and playing gatcha game, seeing my time slowly slipping away. While I enjoy the conept of some me-time and simple rest, after a while, I cannot help but feel bored. Then it comes the realization that I have somewhat been low on motivation. 
+On Wednesday afternoon of the last week of my summer internship, I found myself rotting on my bed and playing gatcha game, seeing my time slowly slipping away. While I enjoy the conept of some me-time and simple rest, after a while, I cannot help but feel bored. It comes the realization that I have somewhat been low on motivation. 
 
-So I did what I always do when I am confused about certain mentalities I have. I opened up my laptop, went on the browser, and typed in "Lost all motivation reddit". I have made this into a habit- whenever I cannot find data for a specific issue in my brain, I consult other peoples' experience. Even if there are no one-size-fit-all solutions provided, at times merely gainging a better understanding of it or seeing that the experience is shared provides food for thoughts and comfort. 
+So I did what I always do when I am uncertain - I opened up my laptop, went on the browser, and typed in "Lost all motivation reddit". I have made this into a habit- whenever I cannot find data for a specific issue from my own brain, I consult other peoples' experience. Even if there isn't a one-size-fit-all solutions, simply understanding the issue better or knowing others share the experience provides food for thoughts and comfort. 
 
-A couple of comments down, I found an interesting article. 
-https://www.butyoudontlooksick.com/articles/written-by-christine/the-spoon-theory/
-The author, Christine, was diagnosed with a rare autoimmune disease, and as her college friend asked her what it "feels" like to have it, she came up with this brilliant metaphor- the spoon theory. 
+A couple of comments down, I found an interesting article. The author, Christine, was diagnosed with a rare autoimmune disease, and as her college friend asked her what it "feels" like to have it, she came up with this brilliant metaphor- the "Spoon Theory". 
 
 > "Imagine we all have a certain numbers of spoons every morning we wake up. We think about this as energy, or the abilty we have to do things- if we use it all up, we get drained." 
 
-For normal people, they usually are born and gifted with a somewhat endless numbers of spoons - everyday, thay can wake up and have minimum concern for how the things they are about to do might affect their body or their mind. Sure, the energy gets depleted, we get tired after a good amount of work, but usually, after some good food and a good 6 sleep cycles of rest, we come back with full health bar again. And not to mention that easy activities such as showering, cooking and other would usually be as easy as flipping to the back of their palms. 
+Using his metaphor, most people are born and gifted with pratically endless numbers of "spoons" - everyday, we wake up and not have concern for how our routine might affect our body or our mind. Sure, energy gets depleted after a good amount of work, but good food and a deent 6-sleep-cycles of rest can grant us a full health bar again. Not to mention that easy activities such as showering, cooking and other would usually be as easy as flipping to the back of our palms. 
 
 But for Christine, things are different. Each of even the easy acivities would cost her a spoon - waking up and opening her eyes would cost her a spoon, getting out of bed would cost a spoon, and taking medicine would only prevent all spoons from losing. 
 
 >  "I explained that I have to see what clothes I can physically put on, if my hands hurt that day buttons are out of the question. If I have bruises that day, I need to wear long sleeves, and if I have a fever I need a sweater to stay warm and so on. If my hair is falling out I need to spend more time to look presentable, and then you need to factor in another 5 minutes for feeling badly that it took you 2 hours to do all this."
 
+My upbringing echos Christine's experience in some ways. I was granted the priviledge of a mostly healthy body - four limbs, decent immune system, and quite a cheerful personality, surrounded by people who love me. But there is one catch. As a premature baby, my parents were warned that I had a high risk of developing amblyopia, and I was given glasses as early as preschool. 
 
+As loving parents of an only child, they were overtaken by the fear that I might not be able to care for myself. They constantly warned me against any form of entertainment that envolves using screens or even reading books, in this digital age when all school-aged kids bonded over movies, animes and games. The sentence "your eyes, you will go blind if you are not careful" was deeply seared into my mind, and it continued to haunt me for 18 years, combined with dread, social isolation, confusion, loathe, and resentment towards my body and my own emotions. Yet, exactly beause of this lack of resoures, I developed tremendous drive for creation. I was dedicated to become a manga artist, because the mangas I have read in secret were the only things that made me happy and seen, and I was eagerly looking forward to the days when my vision stabalizes in my 20s, so that I will have unlimited time and will be free from this curse of worries. But life went on, many things have happened, and fast foward to now, I have forgotten about the anticipation. 
 
+Seeing this article have thrown me back to those good times, when resources were limited, when I lived in anxiety (and as a matter of fact, still do and am still combatting against), but I was able to focus on that tiny bit of joy that doing art gave me. I now hold gratitude that those periods of time was over, and I am finally free to pursue my old hobby as well as newly developed passion, knowing that the old me would have been so happy to have this life. 
+
+Sometimes, it just takes a small reminder. 
 
 [Read Christine Miserandinos's full story here](https://www.butyoudontlooksick.com/articles/written-by-christine/the-spoon-theory/). 
 
-## Links
-[Link text](https://example.com)
-
-## Process for Creating a New Post
-1. Copy this template file
-2. Rename it to your post name (e.g., \`my-new-post.js\`)
-3. Update the metadata
-4. Write your content
-5. Test locally
-6. Commit and push your changes
-`; 
+`;
